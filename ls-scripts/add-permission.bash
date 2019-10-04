6e62e672c88ebef55d1f51f978c7be2693d936c1
damoklov <mishanya@protonmail.com>
2019-10-04 10:35:46 +0300

    Indentation fixes

--- ls-scripts/add-permission.bash
+++ ls-scripts/add-permission.bash
@@ -3,9 +3,9 @@
 
 
 if [ $# -ne 1 ]
-    then 
-    echo "Usage: $0 file"
-    exit 1
+	then 
+  echo "Usage: $0 file"
+  exit 1
 fi
 
 if [ -f $1 ] || [ -d $1 ]
--- ls-scripts/list-info.bash
+++ ls-scripts/list-info.bash
@@ -18,9 +18,9 @@ function checkFile() {
 }
 
 if [ $# -eq 0 ]
-    then 
-    echo "Usage: $0 file1 [file2 ...]"
-    exit 1
+  then
+  echo "Usage: $0 file1 [file2 ...]"
+  exit 1
 fi
 
 for file in "$@"
