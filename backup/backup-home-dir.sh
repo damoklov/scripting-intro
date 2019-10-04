39ac8f1fef1d9fee7e0d83c0c8dd2eeec6d3f040
damoklov <mishanya@protonmail.com>
2019-10-04 16:50:09 +0300

    Minor fixes

--- backup/backup-home-dir.sh
+++ backup/backup-home-dir.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Backups home folder every one minute
+# Place this script not into your home directory!
 # Use the following command with this script:
 # */1 * * * * /path/to/directory/backup-home-dir.sh
 
 
-backuptime=`date +%d-%b-%y-%T`
+backuptime=`date +%d-%b-%y-%H-%M-%S`
 sourcedir=/home/$USER/
-dest=/home/$USER/backup-$backuptime.tar.gz # change destination folder here
+dest=/home/$USER/backup-$backuptime.tar.gz # change destination folder if needed
+
 tar -cpzf $dest $sourcedir
