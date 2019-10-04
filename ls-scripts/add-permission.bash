4fac4790d8c1abc03679f578b0f8fc7898215dff
damoklov <mishanya@protonmail.com>
2019-10-04 10:03:33 +0300

    Added scripts for Task4

---
+++ ls-scripts/add-permission.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Takes single filename as an argument and adds execute permissions if it is a regular file.
+
+
+if [ $# -ne 1 ]
+    then 
+    echo "Usage: $0 file"
+    exit 1
+fi
+
+if [ -f $1 ] || [ -d $1 ]
+	then
+	filetype=$(ls -l -d $1 | cut -b 1)
+	if [ "$filetype" == "-" ]
+		then
+		echo "Regular file"
+		chmod u=w $1
+	else
+		echo "Not a regular file"
+	fi
+else
+	echo "File does not exist"
+fi
---
+++ ls-scripts/list-info.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Does an 'ls -l' command on each of its arguments only if the argument is the name of a regular file.
+
+
+function checkFile() {
+	if [ -f $1 ] || [ -d $1 ]
+		then
+		filetype=$(ls -l -d $1 | cut -b 1)
+		if [ "$filetype" == "-" ]
+			then
+			fileIsRegular=1
+		else
+			fileIsRegular=0
+		fi
+	else
+		fileIsRegular=0
+	fi
+}
+
+if [ $# -eq 0 ]
+    then 
+    echo "Usage: $0 file1 [file2 ...]"
+    exit 1
+fi
+
+for file in "$@"
+do
+	checkFile $file
+	if [ $fileIsRegular -eq 1 ]
+		then
+		ls -l $file
+	fi
+done
