7697707322e9d5b2ce3bf822b9a24ed2e429e0af
damoklov <mishanya@protonmail.com>
2019-10-06 18:45:43 +0300

    Updated backup script

--- backup/backup-home-dir.sh
+++ backup/backup-home-dir.sh
@@ -4,9 +4,30 @@
 # Use the following command with this script:
 # */1 * * * * /path/to/directory/backup-home-dir.sh
 
-
 backuptime=`date +%d-%b-%y-%H-%M-%S`
 sourcedir=/home/$USER/
-dest=/home/$USER/backup-$backuptime.tar.gz # change destination folder if needed
+dest=/home/$USER/backup-conf.tar.gz # change destination folder if needed
 
-tar -cpzf $dest $sourcedir
+
+function checkPresence() {
+  tar tf $dest | while read line
+    do
+     if ! [ -e "/$line" ]
+     then
+       tar -C $(echo "/$line" | sed -r "s/(.+)\/.+/\1/") -xzf $dest $line --transform 's?.*/??g'
+       echo "Backup of $line completed"
+     fi
+   done
+}
+
+
+if ! [ -e $dest ]
+then
+  find $sourcedir -name '*.conf' -o -name '*.config' -print0 | tar -cpzf $dest --null -T -
+  echo "$dest was created"
+  checkPresence
+else
+  checkPresence
+fi
+
+echo 'Done'
