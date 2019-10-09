40eb8d27eb34c13aa47482dd7aae70410ae1c9d8
damoklov <mishanya@protonmail.com>
2019-10-09 20:40:41 +0300

    Modified due to some errors

--- backup/backup-home-dir.sh
+++ backup/backup-home-dir.sh
@@ -4,18 +4,22 @@
 # Use the following command with this script:
 # */1 * * * * /path/to/directory/backup-home-dir.sh
 
-backuptime=`date +%d-%b-%y-%H-%M-%S`
-sourcedir=/home/$USER/
-dest=/home/$USER/backup-conf.tar.gz # change destination folder if needed
+backuptime=`date +%d-%b@%T`
+sourcedir=$HOME
+dest=$HOME/backup-conf.tar.gz # change destination folder if needed
 
+
+function makeBackup() {
+  tar -C /${line%/*} -xzf "${dest}" "${line}" --transform 's?.*/??g' 2>/dev/null
+}
 
 function checkPresence() {
   tar tf $dest | while read line
     do
-     if ! [ -e "/$line" ]
+     if ! [ -e "/${line}" ]
      then
-       tar -C $(echo "/$line" | sed -r "s/(.+)\/.+/\1/") -xzf $dest $line --transform 's?.*/??g'
-       echo "Backup of $line completed"
+       if ! makeBackup ; then mkdir "/${line}" ; fi
+       echo "Backup of ${line} completed"
      fi
    done
 }
@@ -23,11 +27,11 @@ function checkPresence() {
 
 if ! [ -e $dest ]
 then
-  find $sourcedir -path '*/Trash/*' -prune -o -path '*/.cache/*' -prune -o \( -iname \*config -o -iname \*conf \) -print0 | tar -cpzf $dest --null -T -
-  echo "$dest was created"
+  find "${sourcedir}" -path '*/Trash/*' -prune -o -path '*/.cache/*' -prune -o \( -iname \*config -o -iname \*conf \) -print0 | tar -cpzf $dest --null -T -
+  echo "${dest} was created"
   checkPresence
 else
   checkPresence
 fi
 
-echo 'Done'
+echo "Done at $backuptime"
